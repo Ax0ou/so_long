1bc493b8f1360191972d2dad95c715f0e659136f
Axel Alvarade <axel.alvarade@hotmail.fr>
2026-05-14 19:03:31 +0200

    check map wall

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: aalvard <aalvarad@student.42lausanne.ch    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/09 16:43:14 by aalvard           #+#    #+#             */
-/*   Updated: 2026/05/14 17:34:40 by aalvard          ###   ########.fr       */
+/*   Updated: 2026/05/14 19:03:21 by aalvard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,6 +48,11 @@ int	main(int argc, char **argv)
 	{
 		write(1, "Error\nMap error\n", 16);
 		return (1);
+	}
+	if (check_wall(&game) == -1)
+	{
+	    write(1, "Error\nMap error\n", 16);
+	    return (1);
 	}
 	return (0);
 }
