863e896d3aa33853004c77b5403a7c52299e9a94
Axel Alvarade <axel.alvarade@hotmail.fr>
2026-05-14 19:01:56 +0200

    check map wall

--- include/so_long.h
+++ include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: aalvard <aalvarad@student.42lausanne.ch    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/08 15:44:33 by aalvard           #+#    #+#             */
-/*   Updated: 2026/05/14 17:21:51 by aalvard          ###   ########.fr       */
+/*   Updated: 2026/05/14 18:59:15 by aalvard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,5 +44,5 @@ char	**read_map(int fd, char *path);
 int		get_map_size(t_game *game);
 int		check_char(t_game *game);
 int		check_rect(t_game *game);
-
+int 	check_wall(t_game *game);
 #endif
