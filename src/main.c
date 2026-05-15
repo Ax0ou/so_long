6c0386d702e9af137fdfb8c9a86b94d68b73ecb1
Axel Alvarade <axel.alvarade@hotmail.fr>
2026-05-15 17:27:35 +0200

    Fix file

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: aalvard <aalvarad@student.42lausanne.ch    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/09 16:43:14 by aalvard           #+#    #+#             */
-/*   Updated: 2026/05/15 12:40:09 by aalvard          ###   ########.fr       */
+/*   Updated: 2026/05/15 17:23:42 by aalvard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,5 +59,11 @@ int	main(int argc, char **argv)
 		write(1, "Error\nMap error\n", 16);
 		return (1);
 	}
+	if (check_path(&game) == -1)
+	{
+	    write(1, "Error\nMap error\n", 16);
+	    return (1);
+	}
+	free_map(game.map);
 	return (0);
 }
